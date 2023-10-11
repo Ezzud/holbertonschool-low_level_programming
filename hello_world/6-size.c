3901981737fafddcd5f9efbd4506590c74cab026
Ezzud <contact@ezzud.fr>
2023-10-11 02:57:03 -0700

    YA Betty compliancy fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
-	char charV;
-	int intV;
-	long int lintV;
-	long long int llintV;
-	float floatV;
-	printf("Size of a char: %lu byte(s)\n", (unsigned long int)sizeof(charV));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long int)sizeof(intV));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long int)sizeof(lintV));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long int)sizeof(llintV));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long int)sizeof(floatV));	
+	char cV;
+	int iV;
+	long int liV;
+	long long int lV;
+	float fV;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long int)sizeof(cV));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long int)sizeof(iV));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long int)sizeof(liV));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long int)sizeof(lV));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long int)sizeof(fV));	
 
 	return (0);
 }
