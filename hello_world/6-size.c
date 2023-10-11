5f8a53ebdf376f1680cfc515af88693c7ce2f304
Ezzud <contact@ezzud.fr>
2023-10-11 02:55:17 -0700

    Made printf in multiple lines to be more betty compliant

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,7 +12,11 @@ int main(void)
 	long int lintV;
 	long long int llintV;
 	float floatV;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long int)sizeof(charV));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long int)sizeof(intV));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long int)sizeof(lintV));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long int)sizeof(llintV));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long int)sizeof(floatV));	
 
-	printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)\n", (unsigned long int)sizeof(charV), (unsigned long int)sizeof(intV), (unsigned long int)sizeof(lintV), (unsigned long int)sizeof(llintV), (unsigned long int)sizeof(floatV));
 	return (0);
 }
