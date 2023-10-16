91a1a7c1395f66d9da98f293feeb633e115fe018
Ezzud <contact@ezzud.fr>
2023-10-16 06:58:50 -0700

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(toupper(x));
+	}
+	putchar('\n');
+	return (0);
+}
