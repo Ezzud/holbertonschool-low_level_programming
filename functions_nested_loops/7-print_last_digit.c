868d0a76e99e958857fe8d3f72122caeeb8529c7
Ezzud <contact@ezzud.fr>
2023-10-18 03:41:33 -0700

    Task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - Main Entry
+ * @c: Integer we want to test
+ * Return: the last digit of the integer
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
