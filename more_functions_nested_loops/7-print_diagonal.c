f8c6c5bda8b867a9e01001586c8a861e566a5b54
Ezzud <contact@ezzud.fr>
2023-10-20 05:57:52 -0700

    Task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_diagonal - Main Entry
+ * @n: Size of the diagonal
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
