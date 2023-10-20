19728da6fc9bb47b8433295178a7c04e8cbc1c27
Ezzud <contact@ezzud.fr>
2023-10-20 05:17:53 -0700

    Task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_numbers - Main Entry
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar('0' + i);
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,2 +1,4 @@
 int _isupper(char c);
 int _isdigit(int c);
+void print_numbers(void);
+int _putchar(char c);
