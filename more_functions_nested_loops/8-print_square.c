e21f6dddfc8f4aae1e5bfd43e2846c3e674a9e70
Ezzud <contact@ezzud.fr>
2023-10-20 06:06:24 -0700

    Task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_square - Main Entry
+ * @size: Size of the square
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
