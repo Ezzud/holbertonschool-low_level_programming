d6bb0dd056c58c6cd1cd5f214691bfa7adce0fe4
Ezzud <contact@ezzud.fr>
2023-10-20 05:44:25 -0700

    Fix function name

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,17 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * print_numbers - Main Entry
+ * print_most_numbers - Main Entry
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
+	{
 		if (i == 2 || i == 4)
 			continue;
 		_putchar('0' + i);
+	}
 	_putchar('\n');
 }
