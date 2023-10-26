3b89a7a1d9e882f8db2d270f659a493c7551f729
Ezzud <contact@ezzud.fr>
2023-10-26 03:06:21 -0700

    Fix T6 not compiling

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,19 +6,17 @@
  */
 void puts_half(char *str)
 {
-	int length = 0, 
-	int index = 0, 
+	int length = 0;
+	int index = 0;
 	int i = 0;
 
 	while (str[index++])
 		length++;
-
 	if ((length % 2) == 0)
 		i = length / 2;
 	else
 		i = (length + 1) / 2;
-
-	for (index = n; index < length; index++)
+	for (index = i; index < length; index++)
 		_putchar(str[index]);
 	_putchar('\n');
 }
