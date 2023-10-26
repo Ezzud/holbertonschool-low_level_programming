8526ec2e70dc0bdaef238eb0ae24c3eee6272684
Ezzud <contact@ezzud.fr>
2023-10-26 03:04:33 -0700

    Fix variable name

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,7 @@ void puts2(char *str)
 	while (str[index++])
 		length++;
 
-	for (index = 0; index < len; index += 2)
+	for (index = 0; index < length; index += 2)
 		_putchar(str[index]);
 
 	_putchar('\n');
