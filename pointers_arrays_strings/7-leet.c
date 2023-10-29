b20bf04ca70f725ee78aaebdc63b89e2ada6ae69
Ezzud <contact@ezzud.fr>
2023-10-29 13:45:14 -0700

    Changed variable name for betty

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,15 +9,15 @@ char *leet(char *s)
 {
 	int count = 0;
 	int i;
-	int lowercase_letters[] = {97, 101, 111, 116, 108};
-	int uppercase_letters[] = {65, 69, 79, 84, 76};
+	int lower_letters[] = {97, 101, 111, 116, 108};
+	int upper_letters[] = {65, 69, 79, 84, 76};
 	int encoding_number[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (*(s + count) == lowercase_letters[i] || *(s + count) == uppercase_letters[i])
+			if (*(s + count) == lower_letters[i] || *(s + count) == upper_letters[i])
 			{
 				*(s + count) = encoding_number[i];
 				break;
