7696e33c807faebc6059908606b70289505e3db9
Ezzud <contact@ezzud.fr>
2023-11-09 06:44:53 -0800

    Fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,11 +10,11 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *output;
-	int s1_len;
-	int s2_len;
-	int i;
-	int j;
-	int k;
+	unsigned int s1_len;
+	unsigned int s2_len;
+	unsigned int k;
+	unsigned int i;
+	unsigned int j;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		k = s2_len;
 	else
 		k = n;
-	for (j = 0; j <= k; j++)
+	for (j = 0; j < k; j++)
 		output[j] = s2[j];
 	return (output);
 }
