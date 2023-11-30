5172fe17dea3e83b6903a98acd4446808ed1f5a2
Ezzud <contact@ezzud.fr>
2023-11-30 04:06:35 -0800

    Quickfix task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,12 +12,10 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-	dlistint_t *last = *head;
 
 	new_node->n = n;
 	new_node->prev = NULL;
-	new_node->next = last;
-
+	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
 }
