aa8d502e2ad1962f5d6947fba05e03727a42146b
Ezzud <contact@ezzud.fr>
2023-11-30 04:23:24 -0800

    Task 1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,3 +1,31 @@
 #include "main.h"
-
-void print_binary(unsigned long int n);
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * print_binary - Print binary
+ * representation of a number
+ * @n: The number to convert
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
