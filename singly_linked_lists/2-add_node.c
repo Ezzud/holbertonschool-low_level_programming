eb945f1aa450dd2382d0262bbd4420038e5d56d2
Ezzud <contact@ezzud.fr>
2023-11-30 03:51:26 -0800

    Recode task 1

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,17 +11,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = (list_t *)malloc(sizeof(list_t));
-	int len = 0;
-	char *data = strdup(str);
-	
-	for(; *str; str++, len++)
+	list_t *new_node;
+	size_t count;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	for (count = 0; str[count]; count++)
 		;
 
-	new_node->str = data;
-	new_node->len = len;
+	new_node->len = count;
 	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
