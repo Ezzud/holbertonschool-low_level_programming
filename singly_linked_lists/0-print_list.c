9f2742ce162317b3b500f919c174b6af09e1b6e8
Ezzud <contact@ezzud.fr>
2023-11-30 03:56:00 -0800

    Fix print

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,17 +9,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
-	count = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
