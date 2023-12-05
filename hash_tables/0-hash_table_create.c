fdc41d0d3da1a47ad2d899c036b72a8556a10d6a
Ezzud <contact@ezzud.fr>
2023-12-05 03:33:55 -0800

    Task 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Create a 
+ * has table
+ * @size: Size of the hashtable
+ * Return: created hashtable, NULL if error
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable;
+	hash_node_t **hashlist;
+	unsigned long int i;
+
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return (NULL);
+	hashlist = malloc(sizeof(hash_node_t *) * size);
+	if (hashlist == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < size; i++)
+		hashlist[i] = NULL;
+	hashtable->array = hashlist;
+	hashtable->size = size;
+
+	return (hashtable);
+}
